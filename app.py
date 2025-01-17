4faea33aee9198d5ab8c46c6ae3dd228de3e40d2
Muhammadjon <mkushmonov@gmail.com>
2025-01-17 19:24:36 +0900

    Added

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from fastai.vision.all import *
+import plotly.express as px
+
+import pathlib 
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+#title
+st.title("Xayvonlarni Klassifikatsiya Qiluvchi Model")
+
+#upload image
+file = st.file_uploader("Upload an image: ", type = ['gif', 'jpeg', 'jpg', 'png', 'svg'])
+
+if file:
+    st.image(file)
+
+    #pil convert
+    img = PILImage.create(file)
+
+    #model
+    model = load_learner('animal_model.pkl')
+
+    #prediction
+    pred, pred_id, prob = model.predict(img)
+    st.success(f"Prediction: {pred}")
+    st.info(f"Probability: {prob[pred_id]*100:.1f}%")
+
+    #plotting (Visualisation)
+    fig = px.bar(x = prob*100, y = model.dls.vocab)
+    st.plotly_chart(fig)
